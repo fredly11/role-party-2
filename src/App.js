83a0f332c2ae8a52f41202b1169a515ab5b08621
William Buechele <buechelewd@gmail.com>
2021-05-15 21:34:38 -0600

    Can now change party and rooms, add new party, and send messages

--- src/App.js
+++ src/App.js
@@ -5,8 +5,48 @@ import Messages from "./Components/Messages";
 function App() {
   const [parties, setParties] = useState(data);
   const [activeParty, setActiveParty] = useState(0);
+  const [addRoomText, setAddRoomText] = useState("");
 
   const { name, icon, users, rooms, activeRoom } = parties[activeParty];
+
+  const handleRoomClick = (room, e) => {
+    e.preventDefault();
+    const newParties = parties.map((party, j) => {
+      if (j === activeParty) {
+        party.activeRoom = room.id - 1;
+      }
+      return party;
+    });
+    setParties(newParties);
+  };
+
+  const handleAddRoom = (text, e) => {
+    e.preventDefault();
+    const newParties = parties.map((party, j) => {
+      if (j === activeParty) {
+        party.rooms.push({
+          id: party.rooms.length + 1,
+          name: text,
+          rooms: [],
+        });
+      }
+      return party;
+    });
+    console.log(newParties);
+    setParties(newParties);
+  };
+
+  const handleSendMessage = (messages, e) => {
+    e.preventDefault();
+    const newParties = parties.map((party, j) => {
+      if (j === activeParty) {
+        party.rooms[party.activeRoom].messages = messages;
+      }
+      return party;
+    });
+    console.log(newParties);
+    setParties(newParties);
+  };
 
   return (
     <section className="section">
@@ -30,19 +70,48 @@ function App() {
             );
           })}
         </div>
+
         <article className="job-info">
           <h3>{name}</h3>
           <h4>{icon}</h4>
-          {rooms.map((room) => {
+          {rooms.map((room, id) => {
             return (
-              <div key={room.id} className="job-btn">
+              <div
+                key={room.id}
+                className={`job-btn ${id === activeRoom && "active-btn"}`}
+                onClick={(e) => {
+                  {
+                    handleRoomClick(room, e);
+                  }
+                }}
+              >
                 {room.name}
               </div>
             );
           })}
-          <div>
-            <Messages messages={rooms[activeRoom].messages} />
+          <div className="container">
+            <form
+              onSubmit={(e) => {
+                console.log("here");
+                handleAddRoom(addRoomText, e);
+              }}
+            >
+              <input
+                type="text"
+                onChange={(e) => setAddRoomText(e.target.value)}
+                placeholder="Room to add"
+              />
+              <button className="btn" type="submit">
+                Submit
+              </button>
+            </form>
           </div>
+        </article>
+        <article className="job-info">
+          <Messages
+            messages={rooms[activeRoom].messages}
+            handleSendMessage={handleSendMessage}
+          />
         </article>
       </div>
     </section>
--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,15 +1,44 @@
-const Messages = ({ messages }) => {
-  console.log(messages);
+import React, { useState } from "react";
 
-  return messages.map((message) => {
-    return (
-      <div className="message-container" key={message.id}>
-        <h4>{message.user}</h4>
-        <h5>{message.timestamp}</h5>
-        <p>{message.text}</p>
-      </div>
-    );
-  });
+const Messages = ({ messages, handleSendMessage }) => {
+  const [messageText, setMessageText] = useState("");
+  return (
+    <>
+      {messages.map((message) => {
+        return (
+          <div className="message-container" key={message.id}>
+            <h4>{message.user}</h4>
+            <h5>{message.timestamp}</h5>
+            <p>{message.text}</p>
+          </div>
+        );
+      })}
+      <form
+        onSubmit={(e) => {
+          const newMessages = [
+            ...messages,
+            {
+              id: messages.length + 1,
+              user: "Admiral Snackbar",
+              timestamp: "10/15/4031",
+              text: messageText,
+            },
+          ];
+          console.log(newMessages);
+          handleSendMessage(newMessages, e);
+        }}
+      >
+        <input
+          type="text"
+          onChange={(e) => setMessageText(e.target.value)}
+          placeholder="Message"
+        />
+        <button className="btn" type="submit">
+          Send Message
+        </button>
+      </form>
+    </>
+  );
 };
 
 export default Messages;
